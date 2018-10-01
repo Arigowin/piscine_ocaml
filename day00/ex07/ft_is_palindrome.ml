a98afac445c6725e4b3133fbe845fdb64673aad5
Arigowin <dolewski@student.42.fr>
2018-10-01 17:09:35 +0200

    ex07

---
+++ day00/ex07/ft_is_palindrome.ml
@@ -0,0 +1,44 @@
+
+let ft_is_palindrome str =
+    let len = String.length str in
+    let rec loop_str i j =
+        if j < len && i <> j && String.get str i = String.get str j then
+            loop_str (i + 1) (j - 1)
+        else
+            i = len
+    in loop_str 0 len
+
+
+let main () =
+    print_string "radar -> " ;
+    if ft_is_palindrome "radar" then
+        print_endline "True"
+    else
+        print_endline "False"
+
+    ;
+
+    print_string "madam -> " ;
+    if ft_is_palindrome "madam" then
+        print_endline "True"
+    else
+        print_endline "False"
+
+    ;
+
+    print_string "car -> " ;
+    if ft_is_palindrome "car" then
+        print_endline "True"
+    else
+        print_endline "False"
+
+    ;
+
+    print_string " -> " ;
+    if ft_is_palindrome "" then
+        print_endline "True"
+    else
+        print_endline "False"
+
+
+let () = main ()
