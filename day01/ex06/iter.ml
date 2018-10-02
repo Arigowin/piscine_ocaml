e82ea4d8c344491183eba6aa0f196baebcfb445b
Arigowin <dolewski@student.42.fr>
2018-10-02 17:35:11 +0200

    ex06

---
+++ day01/ex06/iter.ml
@@ -0,0 +1,18 @@
+let rec iter f x n  =
+  let rec it n acc =
+    if n < 0 then
+      -1
+    else if n = 0 then
+      acc
+    else
+      it (n - 1) (f acc)
+  in it n x
+
+
+let main () = 
+  print_int (iter (fun x -> x * x) 2 4 );
+  print_char '\n';
+  print_int (iter (fun x -> x * 2) 2 4);
+  print_char '\n'
+
+let () = main ()
