012ced5a59eebd34bc326e622feff82bbe0d096a
Arigowin <dolewski@student.42.fr>
2018-10-03 20:03:46 +0200

    fix

--- day02/ex01/crossover.ml
+++ day02/ex01/crossover.ml
@@ -1,10 +1,10 @@
 
 let crossover l1 l2 =
   let rec browse_list acc = function
-    | ([], _)                       -> acc
-    | (a::b, [])                    -> browse_list acc (b, l2)
-    | (a::_ as t, c::d) when a = c  -> browse_list (a :: acc) (t, d)
-    | (a::_ as t, _::d)             -> browse_list acc (t, d)
+    | ([], _)                           -> acc
+    | (a :: b, [])                      -> browse_list acc (b, l2)
+    | (a :: _ as t, c :: d) when a = c  -> browse_list (a :: acc) (t, d)
+    | (a :: _ as t, _ :: d)             -> browse_list acc (t, d)
   in
   browse_list [] (l1, l2)
 
