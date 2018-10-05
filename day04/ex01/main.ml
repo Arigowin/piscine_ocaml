9ae06045be77a3b6d23f03511c3328e95cef3908
Arigowin <dolewski@student.42.fr>
2018-10-05 13:50:10 +0200

    fix

--- day04/ex01/main.ml
+++ day04/ex01/main.ml
@@ -3,8 +3,10 @@ let main () =
   List.iter print_endline (List.map Value.toStringVerbose Value.all);
   print_endline (Value.toString (Value.next Value.T2));
   print_endline (Value.toString (Value.next Value.King));
-  print_endline (try (Value.toString (Value.next Value.As)) with Invalid_argument(_) -> "No value after As");
-  print_endline (try (Value.toString (Value.previous Value.T2)) with Invalid_argument(_) -> "No value before T2");
+  print_endline (try (Value.toString (Value.next Value.As))
+                 with Invalid_argument(_) -> "No value after As");
+  print_endline (try (Value.toString (Value.previous Value.T2))
+                 with Invalid_argument(_) -> "No value before T2");
   print_endline (Value.toString (Value.previous Value.King));
   print_endline (Value.toString (Value.previous Value.As))
 
