f2d550c76dd16a2dc42fb2dbd5cce1c41c6de9f9
Arigowin <dolewski@student.42.fr>
2018-10-11 14:54:28 +0200

    ex00

--- day08/ex00/main.ml
+++ day08/ex00/main.ml
@@ -5,9 +5,11 @@ let () =
   let p = new Platinum.platinum in
   let a = new Arsenic.arsenic in
   let z = new Zinc.zinc in
+  let n = new Nitrogen.nitrogen in
   print_endline h#to_string;
   print_endline c#to_string;
   print_endline o#to_string;
   print_endline p#to_string;
   print_endline a#to_string;
-  print_endline z#to_string
+  print_endline z#to_string;
+  print_endline n#to_string
---
+++ day08/ex00/nitrogen.ml
@@ -0,0 +1,7 @@
+class nitrogen =
+  object
+    inherit Atom.atom as super
+    method name = "Nitrogen"
+    method symbol = "N"
+    method atomic_number = 7
+  end
