ffc10c28fa05a9ed87679ab656672e5169284598
Arigowin <dolewski@student.42.fr>
2018-10-12 17:53:24 +0200

    ex01

--- day09/ex01/app.ml
+++ day09/ex01/app.ml
@@ -10,9 +10,29 @@ end
 
 module App =
 struct
+  (* type, status, grade*)
   type project = string * string * int
+
   let zero : project = ("", "", 0)
-  let combine (x:project) (y:project) : project = zero
-  let fail (x:project): project = zero
-  let success (x:project): project = zero
+
+  let combine (p1:project) (p2:project) : project =
+    match p1 with
+    | a, b, c  ->
+      (match p2 with
+       | d, e, f ->
+         let average = (c + f) / 2
+         in (a ^ d, (if average > 80 then "succedd" else "failed"), average)
+      )
+
+  let fail (p:project): project =
+    match p with
+    | a, b, c  -> (a, "failed", 0)
+
+  let success (p:project): project =
+    match p with
+    | a, b, c  -> (a, "succed", 80)
 end
+
+let print_proj (p:App.project) : unit =
+  match p with
+  | a, b, c  -> print_endline ("Type: " ^ a ^ " status: " ^ b ^ " grad: " ^ (string_of_int c))
--- day09/ex01/main.ml
+++ day09/ex01/main.ml
@@ -1,2 +1,11 @@
-let () = 
-  print_endline "Coucou"
+let () =
+  let p1 : App.App.project = ("a", "", 0)
+  in let p2 : App.App.project = ("b", "", 0)
+  in let ps = App.App.success p1
+  in let pf = App.App.fail p2
+  in let res = App.App.combine ps pf
+  in App.print_proj res;
+  App.print_proj p1;
+  App.print_proj p2;
+  App.print_proj ps;
+  App.print_proj pf
